225746de6f0056476a7dda54798e6a13fcb2ac53
Emmanuel Obiora <emmanuelobiora11@gmail.com>
2025-05-30 22:48:06 +0100

    fix netlify build error

--- client/app/fund-wallet/page.tsx
+++ client/app/fund-wallet/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic'
+
 import { useState } from "react";
 import { useMutation, gql } from "@apollo/client";
 import { usePaystackPayment } from "react-paystack";
